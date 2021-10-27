d25237e79cf4160f6ee712d7f1669d3d01d892b6
Duc Luu <luuminhduc00@gmail.com>
2021-10-27 20:14:18 +0700

    header

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,18 @@
+import { Layout, PageHeader } from "antd";
+import { Content, Header } from "antd/lib/layout/layout";
 import React from "react";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Layout>
+        <Header color="white">
+          <h1 style={{ color: "white" }}>App</h1>
+        </Header>
+        <Content></Content>
+      </Layout>
+    </div>
+  );
 };
 
 export default Main;
